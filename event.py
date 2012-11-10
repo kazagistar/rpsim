2dfecf8e56202529cbbc38c0dea4ff53c43a8bd5
Jakub Gedeon <kazagistar@thimble.(none)>
2012-11-10 16:21:24 -0700

    finished up Factory

--- event.py
+++ event.py
@@ -14,7 +14,7 @@ class Position:
         self.runner = None
         self.recorder = number in settings["recorders"]
         self.rate = settings["rate"]
-        
+
         # read in the pauses {O(n)}
         pauses = []
         for pos, start, end in settings["pauses"]
@@ -35,7 +35,7 @@ class Position:
         # this way, the number of pauses do not affect the running time complexity
         # unless
         self.pauses = deque(pauses)
-        
+
         # strategy pattern: set strategy given in settings file
         if settings["wait_pause"] == 1:
             self.pauseDelay = _waitStrategy()
@@ -45,20 +45,21 @@ class Position:
             self.blockDelay = _waitStrategy()
         else:
             self.blockDelay = _retryStrategy()
-        
+
     def nextTime(self, time, blocker):
         """ Generate a time to move next, given a current time."""
+        time += nextRandomTime(self.rate)
         if (blocker is not None) and (blocker.time >= time): # current time is blocked
             time = blockDelay(time, blocker.time)
-        
+
         # If the pause has started, go past the pause, remove it, then repeat
         while self.pauses[0][0] < time:
             time = pauseDelay(self.pauses[0][1]
             self.pauses.popleft()
-       
+
     def _waitStrategy(self, time, end):
         return end + nextRandomTime(self.rate)
-    
+
     def _retryStrategy(self, time, end):
         while time < end:
             time += nextRandomTime(self.rate)
@@ -69,10 +70,10 @@ class Runner(Event):
     "next time" to move."""
     def __init__(self, time, pos):
         Event.__init__(self, time)
-        # Start time is stored to measure time spent crossing at the end 
+        # Start time is stored to measure time spent crossing at the end
         self.record = [time]
         self.pos = pos
-        
+
     def run(self, experiment):
         # Record the time if we are at a recorder
         if experiment.positions[self.pos].recorder:
@@ -91,39 +92,42 @@ class Runner(Event):
         self.time = experiment.positions[self.pos].nextTime(self.time, blocker)
         # Requeue
         experiment.pushEvent(self)
-        
+
     def __str__(self):
         return "Runner(position=" + str(self.pos) + ", time=" + str(self.time) + ")"
 
 class Factory(Event):
     """ Creates Runners at the start of the RNA polymeraze """
-    def __init__(self, maxcount):
+    def __init__(self, settings):
         Event.__init__(self, 0)
-        self.maxcount = maxcount
-    
+        self.maxcount = settings["count"]
+        self.rate = settings["rate"] * settings["density"]
+
+
     def run(self, experiment):
         # If there not room, wait to try again. Otherwise, make a new runner
-        for position in experiment.positions[0:experiment.settings.fatness]:
+        for position in experiment.positions[0:experiment.settings["fatness"]]:
             if position.runner is not None:
-                if experiment.settings.wait_block == 1:
+                if experiment.settings["wait_block"] == 1:
                     self.time = position.runner.time
-                    self.time += nextRandomTime(experiment.settings.rate * experiment.density)
+                    self.time += nextRandomTime(self.rate)
                 else:
                     while self.time < position.runner.time:
-                        self.time += nextRandomTime(experiment.settings.rate * experiment.density)
-                
-                heappush(experiment.heap, self)
+                        self.time += nextRandomTime(self.rate)
+
+                experiment.pushEvent(self)
                 return
-                
+
         # Generate a new runner, and give it a proper time to move
         new = Runner(self.time, 0)
-        new.time = new.nextTime(experiment, new.time + nextRandomTime(experiment.settings.rate), experiment.positions[new.pos + experiment.settings.fatness].runner)
+        blocker = experiment.positions[self.pos + expertiment.settings["fatness"]]
+        new.time = experiment.positions[0].nextTime(new.time, blocker)
         experiment.positions[0].runner = new
-        heappush(experiment.heap, new)
+        experiment.pushEvent(new)
         self.maxcount -= 1
         if self.maxcount > 0:
-            heappush(experiment.heap, self)
-            
+            experiment.pushEvent(self)
+
     def __str__(self):
         return "Factory(maxcount=" + str(self.maxcount) + ", time=" + str(self.time) + ")"
 
@@ -138,4 +142,4 @@ class Repeater(Event):
     def run(self, experiment):
         self.function(experiment, self.time)
         self.time += self.repeat_time
-        heappush(experiment.heap, self)
+        experiment.pushEvent(self)
