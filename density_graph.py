a4d3f3a82ce680a838f36bc01e428abb5dd83057
Jakub Gedeon <kazagistar@gmail.com>
2013-07-30 23:59:52 -0600

    Optional parameter that supports save-to-file

--- density_graph.py
+++ density_graph.py
@@ -2,21 +2,17 @@ import pylab
 import sys
 import csv
 
-def list_form(matrix):
-	for x, row in enumerate(matrix):
-		for y, value in enumerate(row):
-			yield x, y, value
-
-
 with open(sys.argv[1] + "/" + "densities.csv", "rb") as f:
 	# load data
 	data = list(map(float, densities) for densities in csv.reader(f, delimiter=','))
 	# averages...
-	pylab.title("Density over time with alpha 0.5")
+	pylab.title("Density over time")
 	pylab.xlabel("Position")
 	pylab.ylabel("Simulation time (sec)")
 	pylab.imshow(data, vmin=0.0, vmax=0.5, aspect="auto")
 	pylab.colorbar().set_label("Particle density")
 	pylab.gca().invert_yaxis()
-	pylab.show()
-
+	if sys.argv[2]:
+		pylab.savefig(sys.argv[2])
+	else:
+		pylab.show()
